0c61369b68614c8ec7df40101aff5ecd46fda25a
pendingmeaning <149383556+pendingmeaning@users.noreply.github.com>
2023-10-30 12:51:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_m7nj8ae", "template_k7ixms9", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
